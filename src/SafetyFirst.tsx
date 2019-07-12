6876438f594306ccb15d1abe17161d06460d1d19
Matt Kaniaris <mkaniaris@gmail.com>
2019-07-11 17:25:20 -0700

    Get babel/ts/webpack up and running...
    and adds a Dropdown implementation.

---
+++ src/SafetyFirst.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export class SafetyFirst<P, S> extends React.Component<P, S> {
+  unMounted_: boolean;
+  isMounted_: boolean;
+  public setState: typeof React.Component.prototype.setState;
+
+  constructor (props: P) {
+    super(props);
+    this.unMounted_ = false;
+    this.isMounted_ = false;
+    const setState_ = SafetyFirst.prototype.setState;
+    this.setState = (...args) => {
+      if (this.unMounted_ || !this.isMounted_) {
+        return false;
+      }
+
+      setState_.call(this, ...args);
+      return true;
+    };
+  }
+
+  componentWillUnmount () {
+    this.unMounted_ = true;
+    this.isMounted_ = false;
+  }
+
+  componentDidMount () {
+    this.isMounted_ = true;
+  }
+}
---
+++ webpack.config.demo.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+
+const path = require('path');
+const webpack = require('webpack');
+
+const CleanPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+const env = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const buildDir = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  mode: env,
+  entry: {
+    main: './src/index.ts',
+    demo: './demo/demo.tsx',
+    html: './demo/index.html',
+  },
+  devServer: {
+    contentBase: './dist',
+    port: 3000,
+  },
+  module: {
+    rules: [
+      { test: /\.html$/, loader: "file-loader?name=[name].[ext]" },
+      {
+        test: /\.ts|\.tsx$/,
+        exclude: [/node_modules/],
+        use: [
+          { loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
+          },
+          { loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                incremental: true,
+              },
+            }
+          }
+        ]
+      },
+    ]
+  },
+  optimization: {
+    minimize: false,
+  },
+  resolve: {
+    extensions: ['.tsx','.ts', ".js"]
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CleanPlugin([
+      `${buildDir}/*`,
+    ]),
+    new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") }),
+    new ManifestPlugin(),
+  ]
+};
